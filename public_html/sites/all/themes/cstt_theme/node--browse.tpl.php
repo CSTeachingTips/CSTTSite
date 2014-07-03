699216be7c13b6329b912d5cba15baeb6e57b23d
lbnelson <lbnelson@hmc.edu>
2014-07-03 09:22:46 -0700

    Adding modules/theming files

--- public_html/sites/all/themes/cstt_theme/node--browse.tpl.php
+++
@@ -1,7 +0,0 @@
-
-			
-<!--Printing the content from the views module -->
-			  <?php $block = block_load('views','browse_all-block');
-              $dummyblock = _block_get_renderable_array(_block_render_blocks(array($block)));
-              print drupal_render($dummyblock); 
-              ?>
