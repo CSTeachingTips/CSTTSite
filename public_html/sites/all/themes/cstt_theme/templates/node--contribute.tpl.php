fc11c64942099e4d84415d37a52885ac13b7012b
SStilson <sstilson@g.hmc.edu>
2014-07-10 14:26:33 -0700

    Updating Contribute formatting

--- public_html/sites/all/themes/cstt_theme/templates/node--contribute.tpl.php
+++ public_html/sites/all/themes/cstt_theme/templates/node--contribute.tpl.php
@@ -1,4 +1,4 @@
 <br> <br>
 <div class="contribute">
-<iframe src="https://docs.google.com/forms/d/1-OUrLuwqu1a2M3daVxw93727u2Jtm2g_P-C0rpwRmlY/viewform?embedded=true" width="760" height="1000" frameborder="0" marginheight="" marginwidth="0">Loading...</iframe>
+<iframe src="https://docs.google.com/forms/d/1-OUrLuwqu1a2M3daVxw93727u2Jtm2g_P-C0rpwRmlY/viewform?embedded=true" scrolling="no" width="760" height="1000">Loading...</iframe>
 </div>
