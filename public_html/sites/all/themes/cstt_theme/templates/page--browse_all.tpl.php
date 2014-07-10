1255587468c2e3bbca80f17713f4c014c8ffd258
SStilson <sstilson@g.hmc.edu>
2014-07-10 14:18:14 -0700

    Updating media query for panels

--- public_html/sites/all/themes/cstt_theme/templates/page--browse_all.tpl.php
+++ public_html/sites/all/themes/cstt_theme/templates/page--browse_all.tpl.php
@@ -46,7 +46,7 @@
 
 
 <!-- just the side panel -->
-<div class="col-xs-12 col-md-3">
+<div class="col-xs-12 col-sm-3">
 <div class="panel panel-default">
 
   <div class="panel-heading">
@@ -65,7 +65,7 @@
 </div>
 <!--- end sidebar -->
 
-<div class="col-xs-12 col-md-9">
+<div class="col-xs-12 col-sm-9">
 
 
 <?php print render($page['content']); ?>
