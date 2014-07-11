6061caa80fb8a9d9ec0167be9f478704de0685da
SStilson <sstilson@g.hmc.edu>
2014-07-11 14:06:17 -0700

    Adding jQuery file

---
+++ public_html/sites/all/themes/cstt_theme/jquery.smooth-scroll.js
@@ -0,0 +1,20 @@
+//File to implement smooth scrolling in jump-to HTML code
+
+jQuery(document).ready(function($) {
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+});
