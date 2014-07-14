7845280c80f4021ac9e3d1831e6090efe6efb510
lbnelson <lbnelson@hmc.edu>
2014-07-14 15:53:28 -0700

    fixing broken css

--- public_html/sites/all/themes/cstt_theme/templates/page--browse_all.tpl.php
+++ public_html/sites/all/themes/cstt_theme/templates/page--browse_all.tpl.php
@@ -29,13 +29,13 @@
               <li><a href="http://csteachingtips.org/contribute">Contribute</a></li>
           </ul>
               
-            <div id = "nav navbar-nav navbar-right">
+            <ul id = "nav navbar-nav navbar-right">
               <?php 
   $block = block_load('views','-exp-clone_of_browse_all-page');
   $dummyblock = _block_get_renderable_array(_block_render_blocks(array($block)));
   print drupal_render($dummyblock);
   ?>           
-           </div>
+           </ul>
              
           
       </div><!-- /.navbar-collapse -->
