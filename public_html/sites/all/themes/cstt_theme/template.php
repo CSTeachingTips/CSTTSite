e1ed7d5a588b21f8a60d9c7d32b4c3fe8aa3460b
lbnelson <lbnelson@hmc.edu>
2014-07-14 12:45:29 -0700

    Fixing login page

--- public_html/sites/all/themes/cstt_theme/template.php
+++ public_html/sites/all/themes/cstt_theme/template.php
@@ -4,3 +4,22 @@
  * @file
  * template.php
  */
+
+
+
+/**
+* Allows the use of user-login.tpl.php to theme the login page
+*/
+function cstt_theme_theme() {
+  $items = array();
+  // create custom user-login.tpl.php
+  $items['user_login'] = array(
+  'render element' => 'form',
+  'path' => drupal_get_path('theme', 'cstt_theme') . '/templates',
+  'template' => 'user-login',
+  'preprocess functions' => array(
+  'cstt_theme_preprocess_user_login'
+  ),
+ );
+return $items;
+}
---
+++ public_html/sites/all/themes/cstt_theme/templates/user-login.tpl.php
@@ -0,0 +1,7 @@
+
+
+<h3> <br> </h3>
+
+<div class = "col-md-2">
+	<?php print drupal_render_children($form) ?>
+</div>
