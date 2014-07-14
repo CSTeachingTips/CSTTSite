622412f33ba9e57526d6b5fbc1c2ce2954a8c2a4
lbnelson <lbnelson@hmc.edu>
2014-07-14 15:08:41 -0700

    Updating search bar

--- public_html/sites/all/themes/cstt_theme/templates/page.tpl.php
+++ public_html/sites/all/themes/cstt_theme/templates/page.tpl.php
@@ -103,14 +103,14 @@
               <li><a href="http://csteachingtips.org/about">About</a></li>
               <li><a href="http://csteachingtips.org/contribute">Contribute</a></li>
           </ul>
-              
-            <div class = "navbar-right">
+          <ul class = "nav navbar-nav navbar-right">    
+          
               <?php 
               $block = block_load('views','-exp-clone_of_browse_all-page');
               $dummyblock = _block_get_renderable_array(_block_render_blocks(array($block)));
               print drupal_render($dummyblock);
               ?>           
-           </div>
+          </ul>
              
           
       </div><!-- /.navbar-collapse -->
