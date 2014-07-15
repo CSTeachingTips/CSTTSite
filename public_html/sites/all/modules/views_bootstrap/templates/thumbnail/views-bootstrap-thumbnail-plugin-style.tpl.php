6b76b5d1459740b189ee0c627f06f8754941bc33
lbnelson <lbnelson@hmc.edu>
2014-07-15 11:44:48 -0700

    Fixing columns on browse all page

--- public_html/sites/all/modules/views_bootstrap/templates/thumbnail/views-bootstrap-thumbnail-plugin-style.tpl.php
+++ public_html/sites/all/modules/views_bootstrap/templates/thumbnail/views-bootstrap-thumbnail-plugin-style.tpl.php
@@ -17,7 +17,7 @@
     <?php foreach ($items as $row): ?>
       <div class="row">
         <?php foreach ($row['content'] as $column): ?>
-          <div class="col col-lg-<?php print $column_type ?>">
+          <div class="col col-md-<?php print $column_type ?>">
             <div class="thumbnail">
               <?php print $column['content'] ?>
             </div>
@@ -30,7 +30,7 @@
 
     <div class="row">
       <?php foreach ($items as $column): ?>
-        <div class="col col-lg-<?php print $column_type ?>">
+        <div class="col col-md-<?php print $column_type ?>">
           <?php foreach ($column['content'] as $row): ?>
             <div class="thumbnail">
               <?php print $row['content'] ?>
