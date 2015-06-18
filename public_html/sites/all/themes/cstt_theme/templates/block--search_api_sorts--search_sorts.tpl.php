ff8148d8d9e729b07e1f87370621ffc8e4d03046
Sangheetha <Sangheetha@users.noreply.github.com>
2015-06-18 13:47:22 -0700

    Installed Search API Sorts and Search API Sorts Widget

---
+++ public_html/sites/all/themes/cstt_theme/templates/block--search_api_sorts--search_sorts.tpl.php
@@ -0,0 +1,8 @@
+<!-- Formats the Search API Search Sorts Field on the Browse All page -->
+
+
+<div class="pull-right"  id = "sortorder">
+<?php print "Sort By" ?>
+<?php print $content ?>
+
+</div>
