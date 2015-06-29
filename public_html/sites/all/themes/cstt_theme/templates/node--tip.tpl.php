d7efaf8e199134d5eee6cc39a15ff3863654e2f6
Sangheetha <Sangheetha@users.noreply.github.com>
2015-06-29 13:12:32 -0700

    Cleaning up images folder

--- public_html/sites/all/themes/cstt_theme/templates/node--tip.tpl.php
+++ public_html/sites/all/themes/cstt_theme/templates/node--tip.tpl.php
@@ -107,18 +107,18 @@
         <!-- Printing the Twitter button -->
         <div style = "float:right;">
         <?php $urlTwitter = "https://twitter.com/share?url=" . $node_url . "&text=" . $title . " http://csteachingtips.org" . $node_url; ?>
-         <a href="<?php echo $urlTwitter; ?>" target="_blank"><img class = "twittershare" src="/../public_html/images/tshare.png" alt="Post to Twitter"/></a>
+         <a href="<?php echo $urlTwitter; ?>" target="_blank"><img class = "twittershare" src="/../public_html/images/twittershare.png" alt="Post to Twitter"/></a>
       
          <!-- Printing the Facebook button -->
          <?php $urlFacebook = "http://www.facebook.com/sharer.php?u=>Facebook" . $node_url; ?>
-         <a href="<?php echo $urlFacebook; ?>" target="_blank"><img class = "facebookshare" src="/../public_html/images/fshare.png" alt="Share on Facebook"/></a>
+         <a href="<?php echo $urlFacebook; ?>" target="_blank"><img class = "facebookshare" src="/../public_html/images/facebookshare.png" alt="Share on Facebook"/></a>
 
          <!-- Printing the Google+ button -->
          <?php $urlGoogle = "https://plus.google.com/share?url=" . "http://csteachingtips.org" . $node_url; ?>
-         <a href="<?php echo $urlGoogle; ?>" target="_blank"><img class = "googleplusshare" src="/../public_html/images/gshare.png" alt="Share on Google+" /></a>  
+         <a href="<?php echo $urlGoogle; ?>" target="_blank"><img class = "googleplusshare" src="/../public_html/images/googleplusshare.png" alt="Share on Google+" /></a>  
          
         <!-- Printing the Copy URL button -->
-         <input type="image" class = "copyURL" src="/../public_html/images/linkicon4.png" value="Copy URL" onclick="showDiv()" style="vertical-align:middle;"/>
+         <input type="image" class = "copyURL" src="/../public_html/images/linkshare.png" value="Copy URL" onclick="showDiv()" style="vertical-align:middle;"/>
 
          <script>
           function showDiv() {
