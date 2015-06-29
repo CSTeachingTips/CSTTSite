03ec9102ea1c418b0bf02c51de48448646eb4fbb
Sangheetha <Sangheetha@users.noreply.github.com>
2015-06-29 11:48:55 -0700

    Removed old front page contents and changed background color

--- public_html/sites/all/themes/cstt_theme/templates/node--home.tpl.php
+++ public_html/sites/all/themes/cstt_theme/templates/node--home.tpl.php
@@ -8,82 +8,18 @@
         <meta name="description" content="Computer Science Teaching Tips is an NSF funded project for providing tips to Computer Science educators. Supported by Harvey Mudd College and Sagefox Consulting.">
 </head>
 
-<body>
 
-
-  <!-- Logo and mission statement -->
-
+<body style="background-color:#E6F5EB;">
   <br>
- <div class="container-fluid">
-  <div class="col-xs-12 col-md-6 col-md-offset-1" id="cstt-description">
 
-    <center>
-    <p>
-      <h2 class = "descriptionalign"><br>Tailor your teaching with our <br> Computer Science Teaching Tips!</h2>
-    </p>
-    <p><a class="btn btn-primary btn-lg" role="button" href = "http://csteachingtips.org/browse-all">Find Tips Now</a></p>
-    </center>
-
-  </div>
+  <div style="margin-bottom:10%;">
     <center> <img alt="CS Teaching Tips Logo" src="http://csteachingtips.org/images/tree.png" id="cstt-icon"> </center>
-</div>
-
-  <!-- End mission statement -->
-
-<!-- Category Panels --> 
-    
-  <div class="container-fluid">
-  <div class="row">
-<br>
-<br>
-
-    <!-- Teaching Content --> 
-
-    <div class="col-xs-12 col-sm-12 col-md-4">
-      <div class="thumbnail">
-        <center>
-            <div class="caption"> 
-              <h3><a href="http://csteachingtips.org/browse-all">Teaching Content</a></h3>
-              <p class="description">Tips to help you engage your students on specific topics and discover where they might have misconceptions.</p><br>
-            </div>
-        </center> 
-      </div> 
-    </div>
-
-
-    <!-- Managing and Assessing--> 
-
-    <div class="col-xs-12 col-sm-12 col-md-4">
-      <div class="thumbnail">
-        <center>
-            <div class="caption">
-              <h3><a href="http://csteachingtips.org/browse-all">Managing & Assessing</a></h3>
-              <p class="description">Tips to establish your classroom as a safe, equitable space for students to grow and receive valuable, constructive feedback.</p><br>
-            </div>
-        </center> 
-      </div>
-    </div>
-
-    <!-- Organizing Curriculum--> 
-
-    <div class="col-xs-12 col-sm-12 col-md-4">
-      <div class="thumbnail">
-        <center>
-          <div class="caption">
-            <h3><a href="http://csteachingtips.org/browse-all">Organizing Curriculum</a></h3>
-            <p class="description">Tips to support you in lesson planning to improve the overall structure of your course and clarify desired student outcomes.</p><br>
-          </div>
-        </center> 
-      </div>
-    </div>
-
-
   </div>
-  </div>
-  <!-- End Category Panels -->
   
 
 </body>
+
+
 
 </html>
 
