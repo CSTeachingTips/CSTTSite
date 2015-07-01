f26bf1c7fc2f4391a255bc6332bf8834421328b7
Sangheetha <Sangheetha@users.noreply.github.com>
2015-07-01 15:17:07 -0700

    Search bar and Tree scales properly for large and medium screens

--- public_html/sites/all/themes/cstt_theme/templates/node--home.tpl.php
+++ public_html/sites/all/themes/cstt_theme/templates/node--home.tpl.php
@@ -12,15 +12,16 @@
 <body style="background-color:#E6F5EB;">
   <br>
 
+  <div class = "container-fluid">
   <div id="cstt-description" style="text-align:center; ">
     
       <h2 class = "descriptionalign" style="padding-bottom:0px; margin-bottom:-10px;" >Tailor your teaching with our <br>Computer Science Teaching Tips! </h2>
    
   </div>
 
-  <div style="text-align:center;margin-bottom:3%;position:relative;">
+  <div style="text-align:center;position:relative">
 
-    <div id = "centralsearch" style="position:absolute;max-width:500px;left:0;right:0;margin: 0 auto;margin-top:160px;z-index:2;">
+    <div id = "centralsearch" >
 
       <?php 
         $block = block_load('views','-exp-centralsearch-page');
@@ -29,10 +30,11 @@
       ?>     
 
     </div>
-
-    <img style = "margin:auto" alt="CS Teaching Tips Logo" src="/../public_html/images/tree1.png" id="cstt-icon"> 
+    <div style="height:70%">
+    <img style = "margin:auto; margin-top:0px height:100%" alt="CS Teaching Tips Logo" src="/../public_html/images/tree2.png" id="cstt-icon"> 
+    </div>
   </div>
-  
+  </div>
 
 </body>
 
