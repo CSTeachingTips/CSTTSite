9f2a73d1026d99923e4720a3893cedaca7caa477
Sangheetha <Sangheetha@users.noreply.github.com>
2015-07-02 13:40:59 -0700

    Cleaned up CSS for front page

--- public_html/sites/all/themes/cstt_theme/templates/node--home.tpl.php
+++ public_html/sites/all/themes/cstt_theme/templates/node--home.tpl.php
@@ -10,47 +10,42 @@
 
 
 <body style="background-color:#E6F5EB;">
-  
-
-
-
   <br>
-
   <div class = "container-fluid">
-  <div id="cstt-description" style="text-align:center; ">
-    
-      <h2 class = "descriptionalign" style="padding-bottom:0px; margin-bottom:-10px;" >Tailor your teaching with our <br>Computer Science Teaching Tips! </h2>
-   
-  </div>
-
-  <div style="text-align:center;position:relative">
-
-    <div id = "centralsearch" >
-    
-
-      <?php 
-        $block = block_load('views','-exp-centralsearch-page');
-        $dummyblock = _block_get_renderable_array(_block_render_blocks(array($block)));
-        print drupal_render($dummyblock);
-      ?>     
-
+    <!-- Mission Statement -->
+    <div id="cstt-description"> 
+        <h2 class = "descriptionalign">Tailor your teaching with our <br>Computer Science Teaching Tips! </h2>     
     </div>
-    <div style="height:70%">
-    <img style = "margin:auto; margin-top:0px height:100%" alt="CS Teaching Tips Logo" src="/../public_html/images/tree2.png" id="cstt-icon"> 
+
+    <!-- Logo and Search Bar -->
+    <div id = "treesearch">
+      <div id = "centralsearch" >
+        <?php 
+          $block = block_load('views','-exp-centralsearch-page');
+          $dummyblock = _block_get_renderable_array(_block_render_blocks(array($block)));
+          print drupal_render($dummyblock);
+        ?>     
+      </div>
+     
+      <img alt="CS Teaching Tips Logo" src="/../public_html/images/tree2.png" id="cstt-icon"> 
+     
     </div>
+
+
+    <!-- Selects Search Bar when page loads, and places placeholder text in it -->
+    <script>
+      var att = document.createAttribute("placeholder");
+      att.value = "Search Tips";
+      document.querySelector("#centralsearch #edit-search-api-views-fulltext").setAttributeNode(att);
+
+      document.querySelector("#centralsearch #edit-search-api-views-fulltext").focus();
+      document.querySelector("#centralsearch #edit-search-api-views-fulltext").select();
+    </script>
+
+    <br>
   </div>
-  </div>
 
-  <!-- Selects Search Bar when page loads, and places placeholder text in it -->
-  <script>
-    var att = document.createAttribute("placeholder");
-    att.value = "Search Tips";
-    document.querySelector("#centralsearch #edit-search-api-views-fulltext").setAttributeNode(att);
-
-    document.querySelector("#centralsearch #edit-search-api-views-fulltext").focus();
-    document.querySelector("#centralsearch #edit-search-api-views-fulltext").select();
-  </script>
-
+  
 </body>
 
 
