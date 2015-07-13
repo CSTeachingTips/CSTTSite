3e0fcc482c5bf9a2a0fac48de30f88af34c2bc02
mknittel <mknittel@g.hmc.edu>
2015-07-13 13:45:08 -0700

    Fivestar is configured

--- public_html/sites/all/themes/cstt_theme/templates/field--field_fivestar_count.tpl.php
+++ public_html/sites/all/themes/cstt_theme/templates/field--field_fivestar_count.tpl.php
@@ -41,12 +41,57 @@
  * @ingroup themeable
  */
 ?>
+<?php
+$tablet_browser = 0;
+$mobile_browser = 0;
+ 
+if (preg_match('/(tablet|ipad|playbook)|(android(?!.*(mobi|opera mini)))/i', strtolower($_SERVER['HTTP_USER_AGENT']))) {
+    $tablet_browser++;
+}
+ 
+if (preg_match('/(up.browser|up.link|mmp|symbian|smartphone|midp|wap|phone|android|iemobile)/i', strtolower($_SERVER['HTTP_USER_AGENT']))) {
+    $mobile_browser++;
+}
+ 
+if ((strpos(strtolower($_SERVER['HTTP_ACCEPT']),'application/vnd.wap.xhtml+xml') > 0) or ((isset($_SERVER['HTTP_X_WAP_PROFILE']) or isset($_SERVER['HTTP_PROFILE'])))) {
+    $mobile_browser++;
+}
+ 
+$mobile_ua = strtolower(substr($_SERVER['HTTP_USER_AGENT'], 0, 4));
+$mobile_agents = array(
+    'w3c ','acs-','alav','alca','amoi','audi','avan','benq','bird','blac',
+    'blaz','brew','cell','cldc','cmd-','dang','doco','eric','hipt','inno',
+    'ipaq','java','jigs','kddi','keji','leno','lg-c','lg-d','lg-g','lge-',
+    'maui','maxo','midp','mits','mmef','mobi','mot-','moto','mwbp','nec-',
+    'newt','noki','palm','pana','pant','phil','play','port','prox',
+    'qwap','sage','sams','sany','sch-','sec-','send','seri','sgh-','shar',
+    'sie-','siem','smal','smar','sony','sph-','symb','t-mo','teli','tim-',
+    'tosh','tsm-','upg1','upsi','vk-v','voda','wap-','wapa','wapi','wapp',
+    'wapr','webc','winw','winw','xda ','xda-');
+ 
+if (in_array($mobile_ua,$mobile_agents)) {
+    $mobile_browser++;
+}
+ 
+if (strpos(strtolower($_SERVER['HTTP_USER_AGENT']),'opera mini') > 0) {
+    $mobile_browser++;
+    //Check for tablets on opera mini alternative headers
+    $stock_ua = strtolower(isset($_SERVER['HTTP_X_OPERAMINI_PHONE_UA'])?$_SERVER['HTTP_X_OPERAMINI_PHONE_UA']:(isset($_SERVER['HTTP_DEVICE_STOCK_UA'])?$_SERVER['HTTP_DEVICE_STOCK_UA']:''));
+    if (preg_match('/(tablet|ipad|playbook)|(android(?!.*mobile))/i', $stock_ua)) {
+      $tablet_browser++;
+    }
+}
+ 
+ $mobile = $tablet_browser > 0 || $mobile_browser > 0;
+?>
+
 <div class="<?php print $classes; ?> fivestar-count-browse-all"<?php print $attributes; ?>>
   <?php if (!$label_hidden): ?>
     <div class="fivestar-count-label"<?php print $title_attributes; ?>><?php print $label ?>:&nbsp;</div>
   <?php endif; ?>
   <center>
-  <div class="fivestar-count-items hidden-sm hidden-xs"<?php print $content_attributes; ?>>
+  <?php if(!$mobile): ?>
+  <div class="fivestar-count-items"<?php print $content_attributes; ?>>
       <?php foreach ($items as $delta => $item): ?>
       <?php $count = render($item);
       if($count == 1) {
@@ -57,7 +102,9 @@
       ?>
       <?php endforeach; ?>
   </div>
-  <div class="fivestar-count-items hidden-md hidden-lg"<?php print $content_attributes; ?>>
+  <?php endif; ?>
+  <?php if($mobile): ?>
+  <div class="fivestar-count-items"<?php print $content_attributes; ?>>
       <?php foreach ($items as $delta => $item): ?>
       <?php $count = render($item);
       if($count == 1) {
@@ -68,5 +115,6 @@
       ?>
       <?php endforeach; ?>
   </div>
+  <?php endif; ?>
   </center>
 </div>
