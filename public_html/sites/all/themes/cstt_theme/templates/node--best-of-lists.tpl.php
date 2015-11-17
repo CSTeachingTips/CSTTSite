453ca719d28945723095284235e7024eaa9bbb21
ArielleBea <ArielleBea@users.noreply.github.com>
2015-11-16 20:18:26 -0500

    add node for best of pages

---
+++ public_html/sites/all/themes/cstt_theme/templates/node--best-of-lists.tpl.php
@@ -0,0 +1,17 @@
+<!-- Best of Lists -->
+
+<html>
+
+<body>
+
+<h1><br><br><br><br><br></h1>
+
+<center>
+<div class="col-xs-12 col-md-8">
+        <!-- Printing the body/additional info field for content type tip -->
+        <?php print render($content['body']); ?>
+      </div>
+</center>
+
+</body>
+</html>
